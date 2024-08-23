e9f302cdae3a8d3a0db39f51fe91119e16916192
Francis <francis.source@gmail.com>
2024-08-23 17:40:52 -0300

    adicionando conteúdo ao projeto

--- src/assets/strings/stringsApp.tsx
+++ src/assets/strings/stringsApp.tsx
@@ -1,7 +1,6 @@
 export const StringsApp = {
   aboutMe:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed diam urna, euismod at porttitor in, dignissim eu mauris. In sem nibh, viverra eu risus sed, accumsan luctus dui. Fusce ex neque, maximus quis dui et, rutrum sollicitudin sem. elit. Sed diam urna, euismod at porttitor in, dignissim eu mauris. In sem nibh, viverra eu risus sed, accumsan luctus dui. Fusce ex neque, maximus quis dui et, rutrum sollicitudin sem.",
-
+    "Meu nome é Francis Santos, atualmente eu sou desenvolvedor na empresa DB. Iniciei minhas atividades na empresa em Fevereiro de 2024, atualmente estou desempenhando atividades de backend de criação de criação de api no padrão REST em Spring Boot com java e atividades de front com React e TypeScript. Sempre estou me mantendo atualizado com as novidades do mercado de tecnologia e buscando aprimorar meus conhecimentos. Sou participativo, comunicativo, muito curioso e sempre buscando a colaboração no trabalho em equipe e ajudar meus colegas. Gosto muito de novos  desafios.",
   aboutMeTitle: "Sobre mim",
 
   skills: "Habilidades",
